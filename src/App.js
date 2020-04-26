c8d6dff82b6a208266c4913fb4a1eac6b4bb4863
pastukhVitalii <pastukh.v.7@gmail.com>
2020-04-26 10:47:49 +0300

    showed names list

--- src/App.js
+++ src/App.js
@@ -2,12 +2,50 @@ import React from 'react';
 import css from './App.module.css';
 import Counter from "./components/Counter/Counter";
 
-function App() {
-  return (
-    <div className={css.app}>
-      <Counter />
-    </div>
-  );
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.addNameRef = React.createRef();
+  }
+
+  state = {
+    count: 0,
+    names: [
+      {name: null}
+    ]
+  };
+
+  onClickFunc = () => {
+    // debugger;
+    let newTitle = this.addNameRef.current.value;
+    let newName = {name: newTitle};
+    let copyNames = [...this.state.names, newName];
+    if (newTitle !== ''  && isNaN(newTitle[0])) {
+      this.setState({names: copyNames});
+      this.setState({count: this.state.count + 1});
+      this.addNameRef.current.value = '';
+    } else {
+      alert("Write name!!! The first character cannot be a number");
+    }
+  };
+
+  render() {
+    let namesArr = this.state.names.map((elem, index) => {
+      if (index > 0) {
+        return <div key={index}> {`${index}.  ${elem.name}`}  </div>
+      }
+    });
+    return (
+      <div className={css.app}>
+        <Counter
+          state={this.state}
+          clickFunc={this.onClickFunc}
+          addNameRef={this.addNameRef}
+          namesArr={namesArr}/>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/Counter/Counter.js
+++ src/components/Counter/Counter.js
@@ -1,35 +1,19 @@
 import React from 'react';
 import css from './Conter.module.css';
+import Number from "./Number/Number";
+import Button from "./Button/Button";
+import InputName from "./InputName/InputName";
+import NameList from "./NameList/NameList";
 
 class Counter extends React.Component {
-
-  constructor(props) {
-    super(props);
-    this.addNameRef = React.createRef();
-  }
-
-  state = {
-    count: 0
-  };
-
-  onClickFunc = () => {
-    if (this.addNameRef.current.value !== '') {
-      alert(this.addNameRef.current.value);
-      this.addNameRef.current.value = '';
-      let newCount = this.state.count + 1;
-      this.setState({count: newCount});
-    } else {
-      alert("Write name!!!");
-      this.addNameRef.current.value = '';
-    }
-  };
 
   render = () => {
     return (
       <div className={css.counter}>
-        <span>{this.state.count}</span>
-        <input type="text" ref={this.addNameRef} placeholder="Write name"/>
-        <button onClick={this.onClickFunc}>Add</button>
+        <Number state={this.props.state} />
+        <InputName addNameRef={this.props.addNameRef}/>
+        <Button state={this.props.state} clickFunc={this.props.clickFunc} />
+        <NameList state={this.props.state} namesArr={this.props.namesArr}/>
       </div>
     );
   }
